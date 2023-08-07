3572e783b483380113aeaa29cf8710b196ec230c
druej-innotech <stephens.druej@gmail.com>
2023-08-07 15:35:31 +0300

    fix: Warning - Interface functions are implicitly virtual

--- contracts/IBridgeNFT.sol
+++ contracts/IBridgeNFT.sol
@@ -10,13 +10,13 @@ interface IBridgeNFT {
     mintArgs are not supposed to be passed to the receiver but rather,
     must be used for custom logic in this contract (if any)
     */
-    function mint(address to, uint256 id, bytes calldata mintArgs) external virtual;
+    function mint(address to, uint256 id, bytes calldata mintArgs) external;
 
     // Burn an NFT
-    function burnFor(address to, uint256 id) external virtual;
+    function burnFor(address to, uint256 id) external;
 
     /* Base URL of the contract
     Must follow the ERC1155Metadata_URI extension's {id} based format
     */
-    function baseURI() external virtual returns (string memory);
+    function baseURI() external returns (string memory);
 }
